27e5f38872edb20ee26ac8ddc1266fdfd561149e
2468785842 <2468785842@qq.com>
2025-02-26 10:02:19 +0800

    full screen

--- android/app/java/org/github/krkr2/MainActivity.java
+++ android/app/java/org/github/krkr2/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -37,7 +38,14 @@ public class MainActivity extends KR2Activity {
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         getWindow().setAttributes(lp);
-
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
         // Initialize Activity Result Launchers
         storagePermissionLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -57,11 +65,6 @@ public class MainActivity extends KR2Activity {
             }
         }
 
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
     }
 
     private boolean checkStoragePermission() {
